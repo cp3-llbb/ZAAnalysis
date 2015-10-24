7396c9226b804e0c57aaf1ea69e195d3532cff22
Alexandre Mertens <alexandre.mertens@cern.ch>
2015-10-24 22:57:50 +0200

    Starting to copy from TTAnalysis :p it compiles

--- plugins/ZAAnalyzer.cc
+++ plugins/ZAAnalyzer.cc
@@ -1,103 +1,211 @@
+#include <cp3_llbb/ZAAnalysis/interface/Types.h>
+#include <cp3_llbb/ZAAnalysis/interface/Tools.h>
+//#include <cp3_llbb/ZAAnalysis/interface/GenStatusFlags.h>
 #include <cp3_llbb/ZAAnalysis/interface/ZAAnalyzer.h>
+#include <cp3_llbb/ZAAnalysis/interface/ZADileptonCategories.h>
+
+#include <cp3_llbb/Framework/interface/MuonsProducer.h>
+#include <cp3_llbb/Framework/interface/ElectronsProducer.h>
+#include <cp3_llbb/Framework/interface/JetsProducer.h>
+#include <cp3_llbb/Framework/interface/METProducer.h>
+#include <cp3_llbb/Framework/interface/HLTProducer.h>
+#include <cp3_llbb/Framework/interface/GenParticlesProducer.h>
+
+#include <Math/PtEtaPhiE4D.h>
+#include <Math/LorentzVector.h>
+#include <Math/VectorUtil.h>
+
+using namespace ROOT::Math;
+
+using namespace ZAAnalysis;
+
+float ZAAnalysis::DeltaEta(const myLorentzVector& v1, const myLorentzVector& v2){
+  return abs(v1.Eta() - v2.Eta());
+}
 
 
-void ZAAnalyzer::analyze(const edm::Event& event, const edm::EventSetup& setup, const ProducersManager& producers, const CategoryManager&) {
+void ZAAnalyzer::analyze(const edm::Event& event, const edm::EventSetup& setup, const ProducersManager& producers, const AnalyzersManager& analyzers, const CategoryManager&) {
 
-/*
-    BRANCH(selectedjets,std::vector<LorentzVector>);
-    BRANCH(selectedbjets,std::vector<LorentzVector>);
-    BRANCH(dijets,LorentzVector);
-    BRANCH(dibjets,LorentzVector);
-    BRANCH(njets, int);
-    BRANCH(nbjets, int);
-*/
+  #ifdef _ZA_DEBUG_
+    std::cout << "Begin event." << std::endl;
+  #endif
 
-    const ElectronsProducer& electrons = producers.get<ElectronsProducer>("electrons");
-    const MuonsProducer& muons = producers.get<MuonsProducer>("muons");
-    const JetsProducer& jets = producers.get<JetsProducer>("jets");
+  electrons_IDIso.resize( LepID::Count * LepIso::Count );
+  muons_IDIso.resize( LepID::Count * LepIso::Count );
+  leptons_IDIso.resize( LepID::Count * LepIso::Count );
 
-    for (unsigned int ielectron = 0 ; ielectron < electrons.p4.size() ; ielectron++){
-        if (electrons.relativeIsoR03_withEA[ielectron] < 0.15 && electrons.p4[ielectron].pt() > 20) {
-            isolatedElectrons.push_back(ielectron);
-        }
-    }
-
-    for (unsigned int imuon = 0 ; imuon < muons.p4.size() ; imuon++){
-        if (muons.relativeIsoR04_withEA[imuon] < 0.10 && muons.p4[imuon].pt() > 20) {
-            isolatedMuons.push_back(imuon);
-        }
-    }
-
-    for( unsigned int ijet = 0 ; ijet < jets.p4.size() ; ijet++ ){
-
-        if (jets.p4[ijet].pt() > 30 && abs(jets.p4[ijet].eta()) < 2.4) {
-            selectedjets.push_back(jets.p4[ijet]);
-            if (jets.getBTagDiscriminant(ijet,"pfCombinedInclusiveSecondaryVertexV2BJetTags") > 0.89 ) {
-                selectedbjets.push_back(jets.p4[ijet]);                
-            }
-        } 
-    }
-
-
-    // temporary definition only, one should keep each combination of b, and not only the highest pt one, I guess.
-
-    if (selectedjets.size() > 2) {
-        dijets = selectedjets[0]+selectedjets[1];
-        if (selectedbjets.size() > 2) {
-            dibjets = selectedbjets[0]+selectedbjets[1];
-        }
-    }
-
-
+  diLeptons_IDIso.resize( LepID::Count * LepIso::Count * LepID::Count * LepIso::Count );
+  
+  selJets_selID_DRCut.resize( LepID::Count * LepIso::Count );
+  selBJets_DRCut_BWP_PtOrdered.resize( LepID::Count * LepIso::Count * BWP::Count );
+  selBJets_DRCut_BWP_CSVv2Ordered.resize( LepID::Count * LepIso::Count * BWP::Count );
 
 /*
-    const ElectronsProducer& electrons = producers.get<ElectronsProducer>("electrons");
-    const MuonsProducer& muons = producers.get<MuonsProducer>("muons");
+  diJets_DRCut.resize( LepID::Count * LepIso::Count );
+  diBJets_DRCut_BWP_PtOrdered.resize( LepID::Count * LepIso::Count * BWP::Count * BWP::Count );
+  diBJets_DRCut_BWP_CSVv2Ordered.resize( LepID::Count * LepIso::Count * BWP::Count * BWP::Count );
+  
+  diLepDiJets_DRCut.resize( LepID::Count * LepIso::Count * LepID::Count * LepIso::Count );
+  diLepDiBJets_DRCut_BWP_PtOrdered.resize( LepID::Count * LepIso::Count * LepID::Count * LepIso::Count * BWP::Count * BWP::Count );
+  diLepDiBJets_DRCut_BWP_CSVv2Ordered.resize( LepID::Count * LepIso::Count * LepID::Count * LepIso::Count * BWP::Count * BWP::Count );
+*/  
+  ///////////////////////////
+  //       ELECTRONS       //
+  ///////////////////////////
 
-    BRANCH(dileptons_mue,std::vector<LorentzVector>);
-    BRANCH(dileptons_mue_indices, std::vector<std::pair<int,int>>);
+  #ifdef _ZA_DEBUG_
+    std::cout << "Electrons" << std::endl;
+  #endif
 
-    for( unsigned int imuon = 0 ; imuon < muons.p4.size() ; imuon++ ){
-        muons.EA_R03[imuon];
-    }
+  const ElectronsProducer& electrons = producers.get<ElectronsProducer>("electrons");
 
-
-
-    //Dilepton muon-electron
-
-    std::cout << " electron size : " << electrons.p4.size() << std::endl;
-    std::cout << " muon size     : " << muons.p4.size() << std::endl;
-
-    for( unsigned int imuon = 0 ; imuon < muons.p4.size() ; imuon++ ){
-        std::cout << "muon is loose? : " << std::endl;
-        if(muons.isLoose[imuon]){
-            std::cout << "   yes " << std::endl;
-            for( unsigned int jmuon = 0 ; jmuon < muons.p4.size() ; jmuon++ ){
-
-                if( muons.p4[jmuon].pt() < muons.p4[imuon].pt() &&
-                    muons.isLoose[jmuon] &&
-                    muons.charge[imuon] * muons.charge[jmuon] < 0 ){
-
-                    LorentzVector dilepton = muons.p4[imuon] + muons.p4[jmuon];
-
-                    if( dilepton.mass() > 20. )
-                    {
-
-                        std::cout << " di lepton candidate " << std::endl;
-
-                        dileptons_mue.push_back(dilepton);
-                        dileptons_mue_indices.push_back(std::make_pair(imuon, jmuon));
-
-                    }
-
-                }
-
-            }
-
+  for(uint16_t ielectron = 0; ielectron < electrons.p4.size(); ielectron++){
+    if( electrons.p4[ielectron].Pt() > m_electronPtCut && abs(electrons.p4[ielectron].Eta()) < m_electronEtaCut ){
+      
+      Lepton m_lepton(
+          electrons.p4[ielectron], 
+          ielectron, 
+          electrons.charge[ielectron], 
+          true, false,
+          electrons.ids[ielectron][m_electronVetoIDName],
+          electrons.ids[ielectron][m_electronLooseIDName],
+          electrons.ids[ielectron][m_electronMediumIDName],
+          electrons.ids[ielectron][m_electronTightIDName],
+          electrons.relativeIsoR03_withEA[ielectron]
+      );
+      
+      for(const LepID::LepID& id: LepID::it){
+        for(const LepIso::LepIso& iso: LepIso::it){ // loop over Iso not really needed since not considered for electrons (for the moment)
+          uint16_t idx = LepIDIso(id, iso);
+          if( m_lepton.ID[id] && m_lepton.iso[iso] )
+            electrons_IDIso[idx].push_back(ielectron);
         }
-
+      }
+      
+      leptons.push_back(m_lepton);
     }
-*/
+  }
+
+  ///////////////////////////
+  //       MUONS           //
+  ///////////////////////////
+  
+  #ifdef _ZA_DEBUG_
+    std::cout << "Muons" << std::endl;
+  #endif
+
+  const MuonsProducer& muons = producers.get<MuonsProducer>("muons");
+
+  for(uint16_t imuon = 0; imuon < muons.p4.size(); imuon++){
+    if(muons.p4[imuon].Pt() > m_muonPtCut && abs(muons.p4[imuon].Eta()) < m_muonEtaCut ){
+      
+      Lepton m_lepton(
+          muons.p4[imuon], 
+          imuon,
+          muons.charge[imuon], 
+          false, true,
+          muons.isLoose[imuon], // isVeto => for muons, re-use isLoose
+          muons.isLoose[imuon],
+          muons.isMedium[imuon],
+          muons.isTight[imuon],
+          muons.relativeIsoR04_withEA[imuon],
+          muons.relativeIsoR04_withEA[imuon] > m_muonLooseIsoCut,
+          muons.relativeIsoR04_withEA[imuon] > m_muonTightIsoCut
+      );
+
+      for(const LepID::LepID& id: LepID::it){
+        for(const LepIso::LepIso& iso: LepIso::it){
+          uint16_t idx = LepIDIso(id, iso);
+          if( m_lepton.ID[id] && m_lepton.iso[iso] )
+            muons_IDIso[idx].push_back(imuon);
+        }
+      }
+
+      leptons.push_back(m_lepton);
+    }
+  }
+
+
+  ///////////////////////////
+  //       DILEPTONS       //
+  ///////////////////////////
+
+  #ifdef _ZA_DEBUG_
+    std::cout << "Dileptons" << std::endl;
+  #endif
+
+  for(uint16_t i1 = 0; i1 < leptons.size(); i1++){
+    for(uint16_t i2 = i1 + 1; i2 < leptons.size(); i2++){
+      const Lepton& l1 = leptons[i1];
+      const Lepton& l2 = leptons[i2];
+
+      DiLepton m_diLepton;
+
+      m_diLepton.p4 = l1.p4 + l2.p4; 
+      m_diLepton.idxs = std::make_pair(l1.idx, l2.idx); 
+      m_diLepton.lidxs = std::make_pair(i1, i2); 
+      m_diLepton.isElEl = l1.isEl && l2.isEl;
+      m_diLepton.isElMu = l1.isEl && l2.isMu;
+      m_diLepton.isMuEl = l1.isMu && l2.isEl;
+      m_diLepton.isMuMu = l1.isMu && l2.isMu;
+      m_diLepton.isOS = l1.charge != l2.charge;
+      m_diLepton.isSF = m_diLepton.isElEl || m_diLepton.isMuMu;
+ 
+      // Save the combination of IDs
+      for(const LepID::LepID& id1: LepID::it){
+        for(const LepID::LepID& id2: LepID::it){
+          uint16_t idx = LepLepID(id1, id2);
+          m_diLepton.ID[idx] = l1.ID[id1] && l2.ID[id2];
+        }
+      }
+
+      // Save the combination of isolations
+      for(const LepIso::LepIso& iso1: LepIso::it){
+        for(const LepIso::LepIso& iso2: LepIso::it){
+          uint16_t idx = LepLepIso(iso1, iso2);
+          m_diLepton.iso[idx] = l1.iso[iso1] && l2.iso[iso2];
+        }
+      }
+      
+      m_diLepton.DR = VectorUtil::DeltaR(l1.p4, l2.p4);
+      m_diLepton.DEta = ZAAnalysis::DeltaEta(l1.p4, l2.p4);
+      m_diLepton.DPhi = VectorUtil::DeltaPhi(l1.p4, l2.p4);
+
+      diLeptons.push_back(m_diLepton);
+    }
+  }
+
+  // Save indices to DiLeptons for the combinations of IDs & Isolationss
+  for(uint16_t i = 0; i < diLeptons.size(); i++){
+    const DiLepton& m_diLepton = diLeptons[i];
+    
+    for(const LepID::LepID& id1: LepID::it){
+      for(const LepID::LepID& id2: LepID::it){
+        for(const LepIso::LepIso& iso1: LepIso::it){
+          for(const LepIso::LepIso& iso2: LepIso::it){
+            
+            uint16_t idx_ids = LepLepID(id1, id2);
+            uint16_t idx_isos = LepLepIso(iso1, iso2);
+            uint16_t idx_comb = LepLepIDIso(id1, iso1, id2, iso2);
+
+            if(m_diLepton.ID[idx_ids] && m_diLepton.iso[idx_isos])
+              diLeptons_IDIso[idx_comb].push_back(i);
+          
+          }
+        }
+      }
+    }
+    
+  }
+
+}
+
+
+void ZAAnalyzer::registerCategories(CategoryManager& manager, const edm::ParameterSet& config) {
+  manager.new_category<ZAAnalysis::ElElCategory>("elel", "Category with leading leptons as two electrons", config);
+  // manager.new_category<TTAnalysis::ElMuCategory>("elmu", "Category with leading leptons as electron, muon", config);
+  //manager.new_category<TTAnalysis::MuElCategory>("muel", "Category with leading leptons as muon, electron", config);
+  manager.new_category<ZAAnalysis::MuMuCategory>("mumu", "Category with leading leptons as two muons", config);
 }
 
 #include <FWCore/PluginManager/interface/PluginFactory.h>
--- test/ZAConfigurationMC.py
+++ test/ZAConfigurationMC.py
@@ -4,31 +4,73 @@ from Configuration.StandardSequences.Eras import eras
 from cp3_llbb.Framework import Framework
 
 process = Framework.create(False, eras.Run2_25ns, '74X_mcRun2_asymptotic_v2', cms.PSet(
-
-    dilepton = cms.PSet(
-        type = cms.string('dilepton_analyzer'),
-        prefix = cms.string('dilepton_'),
-        enable = cms.bool(True),
-        categories_parameters = cms.PSet(
-            mll_cut = cms.untracked.double(20)
-            )
-        ),
-
-
     za = cms.PSet(
         type = cms.string('za_analyzer'),
         prefix = cms.string('za_'),
         enable = cms.bool(True),
+        parameters = cms.PSet(
+            electronPtCut = cms.untracked.double(20),
+            electronEtaCut = cms.untracked.double(2.5),
+            electronVetoIDName = cms.untracked.string('cutBasedElectronID-Spring15-25ns-V1-standalone-veto'),
+            electronLooseIDName = cms.untracked.string('cutBasedElectronID-Spring15-25ns-V1-standalone-loose'),
+            electronMediumIDName = cms.untracked.string('cutBasedElectronID-Spring15-25ns-V1-standalone-medium'),
+            electronTightIDName = cms.untracked.string('cutBasedElectronID-Spring15-25ns-V1-standalone-tight'),
+            
+            muonPtCut = cms.untracked.double(20),
+            muonEtaCut = cms.untracked.double(2.4),
+            muonLooseIsoCut = cms.untracked.double(.20), # Loose cut recommended for dilepton analysis
+            muonTightIsoCut = cms.untracked.double(.12),
+
+            jetPtCut = cms.untracked.double(30),
+            jetEtaCut = cms.untracked.double(2.5),
+            #jetPUID = cms.untracked.double(-9999999),
+            jetDRleptonCut = cms.untracked.double(0.3),
+            jetID = cms.untracked.string('tight'), # not tightLeptonVeto since DeltaR(l,j) cut should be enough
+            jetCSVv2Name = cms.untracked.string('pfCombinedInclusiveSecondaryVertexV2BJetTags'),
+            jetCSVv2L = cms.untracked.double(0.605),
+            jetCSVv2M = cms.untracked.double(0.89),
+            jetCSVv2T = cms.untracked.double(0.97),
+
+            hltDRCut = cms.untracked.double(0.05), # DeltaR cut for trigger matching
+            hltDPtCut = cms.untracked.double(0.1), #Delta(Pt)/Pt cut for trigger matching
+            ),
         categories_parameters = cms.PSet(
-            mll_cut = cms.untracked.double(20)
-            )
+            MllCutSF = cms.untracked.double(20),
+            MllCutDF = cms.untracked.double(20),
+            MllZVetoCutLow = cms.untracked.double(86),
+            MllZVetoCutHigh = cms.untracked.double(116),
+            HLTDoubleMuon = cms.untracked.vstring('HLT_Mu17_TrkIsoVVL_(Tk)?Mu8_TrkIsoVVL_DZ_v.*'),
+            HLTDoubleEG = cms.untracked.vstring('HLT_Ele17_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v.*'),
+            HLTMuonEG = cms.untracked.vstring('HLT_Mu17_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v.*', 'HLT_Mu8_TrkIsoVVL_Ele17_CaloIdL_TrackIdL_IsoVL_v.*'),
+            ),
         )
-    ), redoJEC=True
+    ), redoJEC=False
     )
 
-Framework.schedule(process, ['dilepton','za'])
+Framework.schedule(process, ['za'])
+
+#process.source.firstEvent = cms.untracked.uint32(13083444)
+#process.source.firstLuminosityBlock = cms.untracked.uint32(52386)
+
+# Tricky gen event from /store/mc/RunIISpring15MiniAODv2/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/74X_mcRun2_asymptotic_v2-v1/00000/0014DC94-DC5C-E511-82FB-7845C4FC39F5.root
+# First one is g g -> t tbar with one W -> bbar c
+# Second is b bar -> t tbar semi-leptonic
+#process.source.eventsToProcess = cms.untracked.VEventRange(
+        #'1:52386:13083444',
+        #'1:34020:8496854'
+        #)
+
+# Other tricky gen events, with lots of ISR
+# From file:/nfs/scratch/fynu/swertz/CMSSW_7_4_15/src/cp3_llbb/TTAnalysis/test/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_miniAODv2_oneFile.root
+#process.source.eventsToProcess = cms.untracked.VEventRange(
+        #'1:321521:80300260',
+        #'1:357590:89308562',
+        #'1:387992:96901374'
+        #)
+#process.MessageLogger.cerr.FwkReport.reportEvery = 1
 
 process.source.fileNames = cms.untracked.vstring(
-        'file:///home/fynu/sbrochet/storage/MINIAODSIM/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_Asympt25ns_MCRUN2_74_V9_reduced.root'
+        'file:/nfs/scratch/fynu/swertz/CMSSW_7_4_15/src/cp3_llbb/TTAnalysis/test/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_miniAODv2_oneFile.root'
         )
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(5000))

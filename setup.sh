9ce21320c261892d8377b393ea60f3b170ac9a44
Olivier Bondu <olivier.bondu@gmail.com>
2017-09-25 12:06:28 +0200

    updating setup to be ZA and not HH any longer

--- setup.sh
+++ setup.sh
@@ -7,18 +7,17 @@ then
     echo
     return 1
 fi
-cd ${CMSSW_BASE}/src/cp3_llbb/HHAnalysis
+cd ${CMSSW_BASE}/src/cp3_llbb/ZAAnalysis
 # configure the origin repository
 GITHUBUSERNAME=`git config user.github`
 GITHUBUSERREMOTE=`git remote -v | grep upstream | awk '{print $2}' | head -n 1 | cut -d / -f 2`
 git remote add origin git@github.com:${GITHUBUSERNAME}/${GITHUBUSERREMOTE}
 
 # Add the remaining forks
-git remote add OlivierBondu https://github.com/OlivierBondu/HHAnalysis.git
-git remote add blinkseb https://github.com/blinkseb/HHAnalysis.git
-git remote add delaere https://github.com/delaere/HHAnalysis.git
-git remote add BrieucF https://github.com/BrieucF/HHAnalysis.git
-git remote add swertz https://github.com/swertz/HHAnalysis.git
-git remote add vidalm https://github.com/vidalm/HHAnalysis.git
-git remote add mdelcourt https://github.com/mdelcourt/HHAnalysis.git
+git remote add OlivierBondu https://github.com/OlivierBondu/ZAAnalysis.git
+git remote add delaere https://github.com/delaere/ZAAnalysis.git
+git remote add swertz https://github.com/swertz/ZAAnalysis.git
+git remote add vidalm https://github.com/vidalm/ZAAnalysis.git
+git remote add alesaggio https://github.com/alesaggio/ZAAnalysis.git
+git remote add pieterdavid https://github.com/pieterdavid/ZAAnalysis.git
 
